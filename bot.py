572f785e5c118e3aaf357aadfa2629e99257cc72
Pedro A. Pajarillo Jr <pedropa140@gmail.com>
2024-07-08 16:48:28 -0400

    20240708T16:47:51 - Completed /checkmachine!

--- bot.py
+++ bot.py
@@ -63,7 +63,7 @@ def run_discord_bot():
             print(f'Synced {synced} command(s)')
             print(f'Synced {len(synced)} command(s)')            
             print(f'{bot.user} is now running!')
-            # bot.loop.create_task(checkmachine(bot))
+            bot.loop.create_task(checkmachine(bot))
             bot.loop.create_task(roomchecks(bot))
         except Exception as e:
             print(e)
@@ -298,29 +298,74 @@ def run_discord_bot():
             await asyncio.sleep(60)
 
     async def checkmachine(bot : commands.Bot):
+        history_dictionary = {}
+        for room in room_dictionary:
+            for machine in room_dictionary[room]:
+                if machine not in history_dictionary:
+                    history_dictionary[machine] = {'status': 'UP'}
+        print(history_dictionary)
         while True:
-            # for room in room_dictionary:
-            #     for machine in room_dictionary[room]:
-            #         url = f"https://report.cs.rutgers.edu/nagios4/cgi-bin/status.cgi?style=details&host={machine}"
-            #         page_text = webscraper.fetch_page_content(url).strip('\n')
-            #         webscraper.write_to_file(f"{machine}.txt", page_text)
-            #         current_network_status_output = webscraper.current_network_status(f'{machine}.txt', machine)
-            #         os.remove(f'{machine}.txt')
+            for room in room_dictionary:
+                for machine in room_dictionary[room]:
+                    url = f"https://report.cs.rutgers.edu/nagios4/cgi-bin/status.cgi?style=details&host={machine}"
+                    page_text = webscraper.fetch_page_content(url).strip('\n')
+                    webscraper.write_to_file(f"{machine}.txt", page_text)
+                    current_network_status_output = webscraper.current_network_status(f'{machine}.txt', machine)
+                    os.remove(f'{machine}.txt')
 
-            #         url = f"https://report.cs.rutgers.edu/nagios4/cgi-bin/extinfo.cgi?type=1&host={machine}"
-            #         page_text = webscraper.fetch_page_content(url).strip('\n')
-            #         webscraper.write_to_file(f"{machine}.txt", page_text)
-            #         extended_information_output = webscraper.extended_information(f'{machine}.txt', machine)
-            #         os.remove(f'{machine}.txt')
+                    url = f"https://report.cs.rutgers.edu/nagios4/cgi-bin/extinfo.cgi?type=1&host={machine}"
+                    page_text = webscraper.fetch_page_content(url).strip('\n')
+                    webscraper.write_to_file(f"{machine}.txt", page_text)
+                    extended_information_output = webscraper.extended_information(f'{machine}.txt', machine)
+                    os.remove(f'{machine}.txt')
 
-            #         ilab_machine = IlabMachine(machine, room, extended_information_output[0], current_network_status_output[0],
-            #                                 extended_information_output[1], extended_information_output[2], current_network_status_output[1],
-            #                                 current_network_status_output[2], current_network_status_output[3], current_network_status_output[4],
-            #                                 current_network_status_output[5], current_network_status_output[6], current_network_status_output[7],
-            #                                 current_network_status_output[8], current_network_status_output[9], current_network_status_output[10],
-            #                                 current_network_status_output[11], current_network_status_output[12], current_network_status_output[13])
+                    ilab_machine = IlabMachine(machine, room, extended_information_output[0], current_network_status_output[0],
+                                            extended_information_output[1], extended_information_output[2], current_network_status_output[1],
+                                            current_network_status_output[2], current_network_status_output[3], current_network_status_output[4],
+                                            current_network_status_output[5], current_network_status_output[6], current_network_status_output[7],
+                                            current_network_status_output[8], current_network_status_output[9], current_network_status_output[10],
+                                            current_network_status_output[11], current_network_status_output[12], current_network_status_output[13])
 
-            #         ilab_machine.to_json()
+                    ilab_machine.to_json()
+                    if ilab_machine.host_status.lower() != 'up':
+                        # history_dictionary[machine]['status'] = 'DOWN'
+                        if history_dictionary[machine]['status'].lower() != 'down':
+                            result_title = f'**MACHINE MAY BE DOWN**'
+                            result_description = f'**{machine[0]}**\'s status is currently down.'
+                            embed = discord.Embed(title=result_title, description=result_description, color=13632027)
+                            file = discord.File('images/icon.png', filename='icon.png')
+                            embed.set_thumbnail(url='attachment://icon.png')
+                            embed.set_author(name="CAVE-iLab-Machine-Bot says:")
+                            embed.add_field(name="Machine Name", value= ilab_machine.name, inline=False)
+                            embed.add_field(name="Room Number", value= ilab_machine.room_number, inline=False)
+                            embed.add_field(name="Host Status", value= ilab_machine.host_status, inline=False)
+                            embed.add_field(name="Last Check Time", value= ilab_machine.last_check_time, inline=False)
+                            embed.add_field(name="Next Schedule Active Check", value= ilab_machine.next_schedule_active_check, inline=False)
+                            embed.add_field(name="Is Scheduled Downtime", value= ilab_machine.is_scheduled_downtime, inline=False)
+                            embed.add_field(name="GPU Current Temp", value= ilab_machine.gpu_current_temp, inline=False)
+                            embed.add_field(name="GPU Fans Speed", value= ilab_machine.gpu_fan_speed, inline=False)
+                            embed.add_field(name="Connections", value= ilab_machine.connections, inline=False)
+                            embed.add_field(name="Load", value= ilab_machine.load, inline=False)
+                            embed.add_field(name="Ping", value= ilab_machine.ping, inline=False)
+                            embed.add_field(name="Packet Loss", value= ilab_machine.packet_loss, inline=False)
+                            embed.add_field(name="RTA", value= ilab_machine.rta, inline=False)
+                            embed.add_field(name="Root Disk", value= ilab_machine.root_disk, inline=False)
+                            embed.add_field(name="Smart Failed", value= ilab_machine.smart_failed, inline=False)
+                            embed.add_field(name="Smart Predicted", value= ilab_machine.smart_predicted, inline=False)
+                            embed.add_field(name="SSH", value= ilab_machine.ssh, inline=False)
+                            embed.add_field(name="VarDisk", value= ilab_machine.vardisk, inline=False)
+                            embed.add_field(name="X2GO", value= ilab_machine.x2go, inline=False)
+                            embed.set_footer(text="/roomchecks")
+                            for guild in bot.guilds:
+                                for channel in guild.channels:
+                                    if channel.name.lower() == 'cave-roomchecks-bot' and str(channel.type).lower() == 'text':
+                                        send_message = bot.get_guild(guild.id).get_channel(channel.id)
+                                        await send_message.send(file=file, embed=embed)
+                            history_dictionary[machine]['status'] = 'DOWN'
+                        else:
+                            continue
+
+
             await asyncio.sleep(300)
 
     @bot.tree.command(name = "status", description = "Get a Status of an iLab Machine.")
